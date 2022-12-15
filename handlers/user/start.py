332281c9b974bc04bcb8ad3029b112b6d6a32122
mariusvi <marius@vilkaitis.lt>
2022-12-15 23:47:14 +0200

    start improved

--- handlers/user/start.py
+++ handlers/user/start.py
@@ -1,4 +1,5 @@
 from aiogram import types, Dispatcher
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from bot import dp, bot
 from scraper.scraper import Scraper
 # import asyncio
@@ -7,6 +8,42 @@ from database.models.Users import Users
 
 scraper = Scraper()
 session = get_session()
+
+BANNER = "https://cdn.dribbble.com/users/1373613/screenshots/5385718/media/30cede328265c92d61aa211591e70b62.gif"
+
+START_CAPTION = """<b>Hi!!</b>
+
+<em>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus 
+tempor tristique sagittis. Mauris imperdiet imperdiet enim vel efficitur. 
+
+Fusce elementum sem quis mauris pulvinar, eu tincidunt leo viverra.
+</em>"""
+commands_button = InlineKeyboardButton(text="📖 Commands", callback_data="COMMANDS_BUTTON")
+datasets_button = InlineKeyboardButton(text="🔢 Database", callback_data="DATASETS_BUTTON")
+predictions_button = InlineKeyboardButton(text="🎯 Predictions", callback_data="PREDICTIONS_BUTTON")
+START_KEYBOARD = InlineKeyboardMarkup().add(commands_button).add(datasets_button).add(predictions_button)
+
+
+COMMANDS_CAPTION = """🗒️ <b>Docs for available commands</b>
+
+USER
+• /start: Command for start
+
+• /fetch: Command to fetch data
+
+• /database: Command to fetch data
+
+• /predict: Command to predict price.
+
+ADMIN
+• /scrape: Command for start scraper
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tempor tristique sagittis. 
+"""
+back_button = InlineKeyboardButton(text="⬅️ Back", callback_data="BACK_BUTTON")
+COMMANDS_KEYBOARD = InlineKeyboardMarkup().add(back_button)
+
+
 
 # async def timer_func(message):
 #     for i in range(1):
@@ -23,12 +60,31 @@ async def start_command(message: types.Message):
             session.add(user)
             session.commit()
     try:
-        await bot.send_message(message.from_user.id, "Hello world!")
+        await bot.send_animation(chat_id=message.from_user.id, caption=START_CAPTION, animation=BANNER, reply_markup=START_KEYBOARD, parse_mode="HTML")
     except:
         await message.reply('Find me here! \n t.me/ai_course_test_bot')
 
+async def comands_button_callback(callback: types.CallbackQuery):
+        await callback.answer("Commands")
+        await callback.message.edit_caption(COMMANDS_CAPTION, parse_mode="HTML")
+        await callback.message.edit_reply_markup(COMMANDS_KEYBOARD)
+    
+async def back_button_callback(callback: types.CallbackQuery):
+        await callback.message.edit_caption(START_CAPTION, parse_mode="HTML")
+        await callback.message.edit_reply_markup(START_KEYBOARD)
+
+async def datasets_button_callback(callback: types.CallbackQuery):
+        await callback.answer("Database clicked! 🖱️")
+
+async def predictions_button_callback(callback: types.CallbackQuery):
+        await callback.answer("Predictions clicked! 🖱️")
+
 
 def register_user_start_handlers(dp: Dispatcher):
-    dp.register_message_handler(start_command, commands=['start'])  
+    dp.register_message_handler(start_command, commands=["start"])  
+    dp.register_callback_query_handler(comands_button_callback, lambda query: query.data in ["COMMANDS_BUTTON"])  
+    dp.register_callback_query_handler(back_button_callback, lambda query: query.data in ["BACK_BUTTON"])  
+    dp.register_callback_query_handler(datasets_button_callback, lambda query: query.data in ["DATASETS_BUTTON"])  
+    dp.register_callback_query_handler(predictions_button_callback, lambda query: query.data in ["PREDICTIONS_BUTTON"])  
 
 
